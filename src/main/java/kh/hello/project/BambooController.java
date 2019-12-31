52383372d581b20300139966298beed981fc1cb3
fgdkasdf12 <fgdkasdf12@git.com>
2019-12-31 12:17:55 +0900

    [moon][12-31 AM 12:17] bamboo clean

---
+++ src/main/java/kh/hello/project/BambooController.java
@@ -0,0 +1,13 @@
+package kh.hello.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bamboo/")
+public class BambooController {
+	@RequestMapping("/bamboolist.do")
+	public String bamboolistView () {
+		return "redirect:bamboo/bambooList";
+	}
+}
